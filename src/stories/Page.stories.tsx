934b0a4ed780bd18e66797d6223b022c16f918d6
cancio <filipe.cancio@outlook.com>
2021-03-06 09:53:00 -0300

    removendo funcionalidades desnecessárias

--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { Page, PageProps } from './Page';
-import * as HeaderStories from './Header.stories';
 
 export default {
   title: 'Example/Page',
@@ -14,10 +13,8 @@ const Template: Story<PageProps> = (args) => <Page {...args} />;
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-  ...HeaderStories.LoggedIn.args,
 };
 
 export const LoggedOut = Template.bind({});
 LoggedOut.args = {
-  ...HeaderStories.LoggedOut.args,
 };
